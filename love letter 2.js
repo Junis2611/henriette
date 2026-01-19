f7991392db6fbe4f5e84e0b869366e622a1cb8bb
Junis2611 <lukas.weickert@outlook.com>
2026-01-19 13:15:36 +0100

    Update love letter 2.js

--- love letter 2.js
+++ love letter 2.js
@@ -2,13 +2,21 @@ $(document).ready(function () {
   var envelope = $("#envelope");
   var btn_open = $("#open");
   var btn_reset = $("#reset");
+  var redirectTimeout = null; // Timeout merken
 
   envelope.click(function () {
     open();
   });
+
   btn_open.click(function () {
     open();
+
+    // Weiterleitung nach Animation
+    redirectTimeout = setTimeout(function () {
+      window.location.href = "Page2.html";
+    }, 5000);
   });
+
   btn_reset.click(function () {
     close();
   });
@@ -16,7 +24,14 @@ $(document).ready(function () {
   function open() {
     envelope.addClass("open").removeClass("close");
   }
+
   function close() {
     envelope.addClass("close").removeClass("open");
+
+    // Weiterleitung abbrechen
+    if (redirectTimeout) {
+      clearTimeout(redirectTimeout);
+      redirectTimeout = null;
+    }
   }
 });
